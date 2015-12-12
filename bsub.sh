f8de25fc700e30091f5a675bfbe11914ee6696fb
1753941046 <lun01@uk-cri-lcst01.crnet.org>
2015-12-12 17:46:35 +0000

    Updated bsub script.

--- bsub.sh
+++ bsub.sh
@@ -1,6 +1,19 @@
+cd quantile
 bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save quantile.R
-bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save power.R
-bsub -R "rusage[mem=5000]" -n 6 -e "log.err" -o "log.out" R CMD BATCH --no-save failsim.R
-bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save alternatives.R
-bsub -R "rusage[mem=5000]" -n 1 -e "log_de.err" -o "log_de.out" R CMD BATCH --no-save de_analyzer.R
+cd -
 
+cd power
+bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save power.R
+cd -
+
+cd failsim
+bsub -R "rusage[mem=5000]" -n 6 -e "log.err" -o "log.out" R CMD BATCH --no-save failsim.R
+cd -
+
+cd alternatives
+bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save alternatives.R
+cd - 
+ 
+cd realdata
+bsub -R "rusage[mem=5000]" -n 1 -e "log_de.err" -o "log_de.out" R CMD BATCH --no-save de_analyzer.R
+cd - 
