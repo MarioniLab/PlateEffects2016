f033651a61d48723fafa770137a147f26804a2da
Aaron Lun <alun@wehi.edu.au>
2016-04-06 12:48:25 +0100

    Added code to get uncontrolled results.

--- realdata/submitter.sh
+++ realdata/submitter.sh
@@ -9,4 +9,6 @@ bsub -R "rusage[mem=5000]" -n 1 -e "ESpresso/log_real.err" -o "ESpresso/log_real
 bsub -R "rusage[mem=5000]" -n 1 -e "ESpresso/log_scramble.err" -o "ESpresso/log_scramble.out" Rdevel CMD BATCH --no-save scramble_ESpresso.R
 bsub -R "rusage[mem=5000]" -n 1 -e "ESpresso/log_bulk.err" -o "ESpresso/log_bulk.out" Rdevel CMD BATCH --no-save bulk_ESpresso.R
 
+# Results aren't under version control, so they need to be pulled down manually:
+# rsync -azv cruk:lustre/PlateEffects/realdata/ESpresso/ ESpresso/
 
