03a95dee71f5ae89f15828adbbfe52ed5442deba
Aaron Lun <alun@wehi.edu.au>
2016-04-07 15:17:59 +0100

    Added updater code to submitter script.

--- reference/submitter.sh
+++ reference/submitter.sh
@@ -2,3 +2,6 @@ echo '
 source("ESpresso.R")
 source("estimateParam.R")
 ' | Rdevel --no-save
+
+# Upload script.
+# rsync -azv results_ESpresso/ cruk:lustre/PlateEffects/reference/results_ESpresso/ --delete
