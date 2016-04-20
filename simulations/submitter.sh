1430326d9c23ee4df2d48a5614f8f0e4d304e7b3
Aaron Lun <alun@wehi.edu.au>
2016-04-20 09:31:19 +0100

    Cleaned up submitter file.

--- simulations/submitter.sh
+++ simulations/submitter.sh
@@ -1,9 +1,9 @@
-mkdir ESpresso
+mkdir -p ESpresso
 
 ###########################
 # Error simulations
 
-for logfile in $(find ESpresso/ | grep "log_fail")
+for logfile in $(find ESpresso/ -regex .*/log_fail.*\.)
 do
     rm $logfile
 done
@@ -16,10 +16,15 @@ bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail4.err" -o "ESpresso/log_fai
 bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail5.err" -o "ESpresso/log_fail5.out" R CMD BATCH --no-save "--args scenario=5 $failargs" failsim.R ESpresso/failsim_5.Rout
 bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail6.err" -o "ESpresso/log_fail6.out" R CMD BATCH --no-save "--args scenario=6 $failargs" failsim.R ESpresso/failsim_6.Rout
 
-# This takes intolerably long on a single run, so I'm going to split it up across many cores.
+# This scenario takes intolerably long on a single run, so I'm going to split it up across many cores.
 tmpdir=ESpresso/tmp7
-mkdir $tmpdir
-rm $tmpdir/*.err $tmpdir/*.out
+mkdir -p $tmpdir
+for logfile in $(find $tmpdir -regex .*/log_fail.*\.)
+do
+    rm $logfile
+done
+
+collected=""
 for i in {1..10}
 do
     if [ $i -eq 1 ]
@@ -30,24 +35,40 @@ do
         ncores=1
         longrun=FALSE
     fi
-    mkdir $tmpdir/$i
-    bsub -R "rusage[mem=10000]" -n $ncores -e "${tmpdir}/log_fail7_${i}.err" -o "${tmpdir}/log_fail7_${i}.out" \
+    mkdir -p $tmpdir/$i
+    newname="jobsim7_${i}"
+    bsub -J $newname -R "rusage[mem=10000]" -n $ncores -e "${tmpdir}/log_fail7_${i}.err" -o "${tmpdir}/log_fail7_${i}.out" \
         R CMD BATCH --no-save "--args seed=${i}0000 scenario=7 niter=1 do.long=${longrun} indir='../reference/results_ESpresso' outdir='${tmpdir}/${i}'"\
         failsim.R $tmpdir/failsim_7_${i}.Rout
+
+    if [ $i -eq 1 ]
+    then
+        collected="done($newname)"
+    else
+        collected="$collected && done($newname)"
+    fi
 done
 
-rm ESpresso/results_failsim/*_7.txt
-tmpdir=ESpresso/tmp7
+# Compiling it into a single file.
+echo '
+for file7 in $(find ESpresso/results_failsim -regex .*_7.txt)
+do
+    rm $file7
+done
 for i in {1..10}
 do
-    cat ${tmpdir}/${i}/raw_7.txt >> ESpresso/results_failsim/raw_7.txt
-    cat ${tmpdir}/${i}/sum_7.txt >> ESpresso/results_failsim/sum_7.txt
+    cat ESpresso/tmp7/${i}/raw_7.txt >> ESpresso/results_failsim/raw_7.txt
+    cat ESpresso/tmp7/${i}/sum_7.txt >> ESpresso/results_failsim/sum_7.txt
 done
+rm -r ESpresso/tmp7
+' > temp.sh
+bsub -J compiler -w "$collected" bash temp.sh
+bsub -w "done(compiler)" rm temp.sh
 
 ############################
 # Power simulations.
 
-for logfile in $(find ESpresso/ | grep "log_pow")
+for logfile in $(find ESpresso/ -regex .*/log_pow.*\.)
 do
     rm $logfile
 done
