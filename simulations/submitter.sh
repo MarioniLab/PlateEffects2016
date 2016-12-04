2bf0530fca0405306e2bd5bc708f5710b472ba27
Aaron Lun <alun@wehi.edu.au>
2016-12-04 13:37:32 +0000

    Updated version numbers, submission script.

--- simulations/submitter.sh
+++ simulations/submitter.sh
@@ -9,13 +9,13 @@ do
 done
 
 failargs="seed=10000 indir='../reference/results_ESpresso' outdir='ESpresso/results_failsim'"
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail1.err" -o "ESpresso/log_fail1.out" R CMD BATCH --no-save "--args scenario=1 $failargs" failsim.R ESpresso/failsim_1.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail2.err" -o "ESpresso/log_fail2.out" R CMD BATCH --no-save "--args scenario=2 $failargs" failsim.R ESpresso/failsim_2.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail3.err" -o "ESpresso/log_fail3.out" R CMD BATCH --no-save "--args scenario=3 $failargs" failsim.R ESpresso/failsim_3.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail4.err" -o "ESpresso/log_fail4.out" R CMD BATCH --no-save "--args scenario=4 $failargs" failsim.R ESpresso/failsim_4.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail5.err" -o "ESpresso/log_fail5.out" R CMD BATCH --no-save "--args scenario=5 $failargs" failsim.R ESpresso/failsim_5.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail6.err" -o "ESpresso/log_fail6.out" R CMD BATCH --no-save "--args scenario=6 $failargs" failsim.R ESpresso/failsim_6.Rout
-bsub -R "rusage[mem=5000]" -n 6 -e "ESpresso/log_fail8.err" -o "ESpresso/log_fail8.out" R CMD BATCH --no-save "--args scenario=8 $failargs" failsim.R ESpresso/failsim_8.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail1.err" -o "ESpresso/log_fail1.out" R CMD BATCH --no-save "--args scenario=1 $failargs" failsim.R ESpresso/failsim_1.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail2.err" -o "ESpresso/log_fail2.out" R CMD BATCH --no-save "--args scenario=2 $failargs" failsim.R ESpresso/failsim_2.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail3.err" -o "ESpresso/log_fail3.out" R CMD BATCH --no-save "--args scenario=3 $failargs" failsim.R ESpresso/failsim_3.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail4.err" -o "ESpresso/log_fail4.out" R CMD BATCH --no-save "--args scenario=4 $failargs" failsim.R ESpresso/failsim_4.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail5.err" -o "ESpresso/log_fail5.out" R CMD BATCH --no-save "--args scenario=5 $failargs" failsim.R ESpresso/failsim_5.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail6.err" -o "ESpresso/log_fail6.out" R CMD BATCH --no-save "--args scenario=6 $failargs" failsim.R ESpresso/failsim_6.Rout
+bsub -R "rusage[mem=10000]" -n 6 -e "ESpresso/log_fail8.err" -o "ESpresso/log_fail8.out" R CMD BATCH --no-save "--args scenario=8 $failargs" failsim.R ESpresso/failsim_8.Rout
 
 ###########################
 # This scenario takes intolerably long on a single run, so I'm going to split it up across many cores.
@@ -33,14 +33,16 @@ do
     if [ $i -eq 1 ]
     then
         ncores=6
+        memory=20000
         longrun=TRUE
     else
         ncores=1
+        memory=10000
         longrun=FALSE
     fi
     mkdir -p $tmpdir/$i
     newname="jobsim7_${i}"
-    bsub -J $newname -R "rusage[mem=10000]" -n $ncores -e "${tmpdir}/log_fail7_${i}.err" -o "${tmpdir}/log_fail7_${i}.out" \
+    bsub -J $newname -R "rusage[mem=${memory}]" -n $ncores -e "${tmpdir}/log_fail7_${i}.err" -o "${tmpdir}/log_fail7_${i}.out" \
         R CMD BATCH --no-save "--args seed=${i}0000 scenario=7 niter=1 do.long=${longrun} indir='../reference/results_ESpresso' outdir='${tmpdir}/${i}'"\
         failsim.R $tmpdir/failsim_7_${i}.Rout
 
